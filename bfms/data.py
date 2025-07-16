b26eb5fec271bde265a28db74097a4d6a0592fc3
JunHo Seo <junhoseo@korea.ac.kr>
2025-07-16 06:05:41 +0000

    Add MuJoCo dataset.

--- bfms/data.py
+++ bfms/data.py
@@ -1,8 +1,62 @@
 from pathlib import Path
 
+import minari
 import numpy as np
 from numpy import typing as npt
 from tqdm import tqdm
+
+DATASET_REGISTRY = {
+    "mujoco/halfcheetah/medium-v0": "D4RL",
+    "walker/rnd": "ExoRL",
+}
+
+
+class D4RLDataset:
+    def __init__(self, group: str, env: str, data_quality: str):
+        self._dataset_id = f"{group}/{env}/{data_quality}"
+        self._storage = self._load()
+        self._np_rng = None
+
+    def __len__(self):
+        return len(self._storage["observation"])
+
+    def __getitem__(self, key: str) -> npt.NDArray[np.float32]:
+        return self._storage[key]
+
+    def seed(self, seed: int) -> None:
+        self._np_rng = np.random.default_rng(seed)
+
+    def sample(self, num_transitions: int) -> dict[str, npt.NDArray[np.float32]]:
+        if self._np_rng is None:
+            # 294 is my favorite number.
+            self.seed(seed=294)
+
+        assert self._np_rng is not None
+
+        rand_indexes = self._np_rng.integers(low=0, high=len(self), size=num_transitions)
+        batch = {key: self._storage[key][rand_indexes] for key in self._storage}
+        return batch
+
+    def _load(self) -> dict[str, npt.NDArray[np.float32]]:
+        self._raw_dataset = minari.load_dataset(self._dataset_id)
+        episodes = {
+            "observation": [],
+            "action": [],
+            "next_observation": [],
+            "reward": [],
+            "terminated": [],
+        }
+        for episode in tqdm(self._raw_dataset, desc="Reading Data"):
+            episodes["observation"].append(episode.observations[:-1])
+            episodes["action"].append(episode.actions)
+            episodes["next_observation"].append(episode.observations[1:])
+            episodes["reward"].append(episode.rewards)
+            episodes["terminated"].append(episode.terminations)
+
+        storage = {}
+        for key in episodes:
+            storage[key] = np.concat(episodes[key], dtype=np.float32)
+        return storage
 
 
 class ExoRLDataset:
--- main.py
+++ main.py
@@ -1,60 +1,77 @@
 import jax
+import numpy as np
 import optax
 import tyro
 from dm_control import suite
 from flax import nnx
 from jax import numpy as jnp
 
-from bfms.data import ExoRLDataset
+from bfms.data import DATASET_REGISTRY, D4RLDataset, ExoRLDataset
 from bfms.reward_based import bc
 
 
 def main(
     dataset_dir: str,
-    domain: str,
-    collection_method: str,
+    dataset_id: str,
     eval_task: str,
     seed: int = 294,
     train_step: int = int(1e6),
     batch_size: int = 256,
 ):
-    exorl_dataset = ExoRLDataset(dataset_dir, domain, collection_method)
-    print(f"Dataset {domain}/{collection_method} loaded with {len(exorl_dataset):,} transitions.")
+    if DATASET_REGISTRY[dataset_id] == "D4RL":
+        _group, _env, _data_quality = dataset_id.split("/")
+        dataset = D4RLDataset(_group, _env, _data_quality)
+    elif DATASET_REGISTRY[dataset_id] == "ExoRL":
+        _domain, _collection_method = dataset_id.split("/")
+        dataset = ExoRLDataset(dataset_dir, _domain, _collection_method)
+    print(f"Dataset {dataset_id} loaded with {len(dataset):,} transitions.")
 
     key = jax.random.PRNGKey(seed=seed)
     key, nnx_rngs = jax.random.split(key)
     nnx_rngs = nnx.Rngs(nnx_rngs)
-    exorl_dataset.seed(seed)
+    dataset.seed(seed)
 
-    dim_state = exorl_dataset["observation"].shape[-1]
-    dim_action = exorl_dataset["action"].shape[-1]
+    dim_state = dataset["observation"].shape[-1]
+    dim_action = dataset["action"].shape[-1]
     continuous_bc = bc.ContinuousBC(dim_state, dim_action, rngs=nnx_rngs)
     optimizer = nnx.Optimizer(continuous_bc, optax.adam(1e-3))
 
     for t in range(train_step):
-        batch = exorl_dataset.sample(batch_size)
+        batch = dataset.sample(batch_size)
         batch = jax.tree.map(lambda x: jnp.asarray(x), batch)
         loss = bc.train_step(continuous_bc, optimizer, batch)
         if t % 100 == 0:
             print(f"{t}: loss={loss.item()}")
 
     score = 0.0
-    env = suite.load(domain_name=domain, task_name=eval_task)
-    timestep = env.reset()
-    while not timestep.last():
-        key, action_key = jax.random.split(key)
-        observation = jnp.concat(
-            [
-                timestep.observation["orientations"],
-                jnp.array([timestep.observation["height"]]),
-                timestep.observation["velocity"],
-            ]
-        )
+    if DATASET_REGISTRY[dataset_id] == "D4RL":
+        done = False
+        env = dataset._raw_dataset.recover_environment()
+        observation, _ = env.reset()
+        while not done:
+            key, action_key = jax.random.split(key)
+            action = bc.act(continuous_bc, observation, key=action_key)
+            observation, reward, terminated, truncated, _ = env.step(np.asarray(action))
+            done = terminated or truncated
+            score += float(reward)
+    elif DATASET_REGISTRY[dataset_id] == "ExoRL":
+        _domain, _collection_method = dataset_id.split("/")
+        env = suite.load(domain_name=_domain, task_name=eval_task)
+        timestep = env.reset()
+        while not timestep.last():
+            key, action_key = jax.random.split(key)
+            observation = jnp.concat(
+                [
+                    timestep.observation["orientations"],
+                    jnp.array([timestep.observation["height"]]),
+                    timestep.observation["velocity"],
+                ]
+            )
 
-        action = bc.act(continuous_bc, observation, key=action_key)
-        timestep = env.step(action)
-        score += timestep.reward
-    print(f"Return on {domain}-{eval_task}: {score}")
+            action = bc.act(continuous_bc, observation, key=action_key)
+            timestep = env.step(action)
+            score += timestep.reward
+    print(f"Return on {dataset_id}: {score}")
 
 
 if __name__ == "__main__":
